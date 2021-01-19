561bfd749867f85b8cdc256d255eb67c32f21719
Omar Adel <omarzero1015@gmail.com>
2021-01-20 01:46:14 +0200

    add toolbar and custom chips

---
+++ app/src/main/java/omar/az/fresh/HomeFragment.kt
@@ -0,0 +1,13 @@
+package omar.az.fresh
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/omar/az/fresh/MainActivity.kt
+++ app/src/main/java/omar/az/fresh/MainActivity.kt
@@ -2,10 +2,15 @@ package omar.az.fresh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.mainActivityFrameContainer, HomeFragment()).commit()
     }
 }
