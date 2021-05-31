0af59ef2342d9fab8ea7d37a662249ba1b668c4f
omarzer0 <omarzero1015@gmail.com>
2021-05-31 19:57:45 +0200

    prevent Toast overlap

--- app/src/main/java/omar/az/fresh/utils/Utils.kt
+++ app/src/main/java/omar/az/fresh/utils/Utils.kt
@@ -1,5 +1,15 @@
 package omar.az.fresh.utils
 
+import android.content.Context
+import android.widget.Toast
+
 object Utils {
     const val whiteColor = "#FFFFFF"
+    private var mToast : Toast? = null
+
+    fun showMyToast(context: Context, text:String){
+        mToast?.cancel()
+        mToast = Toast.makeText(context,text, Toast.LENGTH_SHORT)
+        mToast?.show()
+    }
 }
